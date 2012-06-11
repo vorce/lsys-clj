de205e1ef88bb728c87d3147b61c856393d9167b
Joel Carlbark <joelcb@gmail.com>
2012-06-11 21:25:34 +0200

    Minor tweaks. Corrected test cases hueheu

--- src/SpotsNTris/core.clj
+++ src/SpotsNTris/core.clj
@@ -1,18 +1,18 @@
+; L-system
+; 2012, Joel Carlbark
+
 (ns SpotsNTris.core
     (:use quil.core)
     (:gen-class))
-
-; Example:
-; Axiom: F
-; Rule: "F+F-F-F+F"
 
 (defn setup []
     (smooth)
     (frame-rate 1)
     (background 200)
-    (stroke 0)
-    (stroke-weight (random 6)))
+    (stroke 50 75 120))
 
+; Go forward d pixels: "F",
+; returns the altered state
 (defn forward [state d]
     (let [x (+ (get state "x") (* d (cos (get state "a")))),
             y (+ (get state "y") (* d (sin (get state "a")))),
@@ -24,30 +24,41 @@
     ;"y" (+ (get state "y") (* d (sin (get state "a")))),
     ;"a" (get state "a")})
 
+; Turn right d degrees: "-". Returns altered state
 (defn right [state d]
     {"x" (get state "x"),
     "y" (get state "y"),
     "a" (+ (get state "a") (radians d))})
 
+; Turn left d degrees, "+". return the aleterd state
 (defn left [state d]
     {"x" (get state "x"),
     "y" (get state "y"),
     "a" (- (get state "a") (radians d))})
 
+; parse a single command/character
+; in a certain state s
+; Supported commands: F, + and -
 (defn parse-cmd [s cmd]
     (if (.equals cmd "F") (forward s 20)
         (if (.equals cmd "+") (left s 90)
             (if (.equals cmd "-") (right s 90)))))
 
+; Parse the whole string of commands
+; in the state s
 (defn parse [s cmds]
     (if (zero? (count cmds)) s
         (recur (parse-cmd s (str (first cmds))) (apply str (rest cmds)))))
 
+; Produce a new command string from the axiom string
+; and the rules applied to it
 (defn produce [axiom rules]
     (apply str
         (seq (for [i axiom]
             (get rules (str i) (str i))))))
 
+; Recursively apply the rules to the result of a produced
+; command string
 (defn evolve [n axiom rules]
     (if (zero? n) axiom (recur (dec n) (produce axiom rules) rules)))
 
@@ -58,7 +69,7 @@
     ;(parse {"x" 0, "y" 150, "a" (radians 0)} "F-FF+FF"))
 
 (defsketch spots-n-tris
-    :title "SpotsNTris"
+    :title "Clojure L-system"
     :setup setup
     :draw draw
     :size [600 300])
--- test/SpotsNTris/test/core.clj
+++ test/SpotsNTris/test/core.clj
@@ -6,10 +6,9 @@
 ;  (is false "No tests have been written."))
 
 (deftest produce-f
-    (.equals "F+F--F+F--F+F--F+F--F+F--F+F" (produce "F--F--F" {"F" "F+F--F+F"})))
+    (is true (.equals "F+F--F+F--F+F--F+F--F+F--F+F" (produce "F--F--F" {"F" "F+F--F+F"}))))
 
 (deftest produce-koch
-    (.equals "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F" (evolve 2 "F" {"F" "F+F-F-F+F"})))
+    (is true (.equals "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F" (evolve 2 "F" {"F" "F+F-F-F+F"}))))
 
-;(deftest 
 
